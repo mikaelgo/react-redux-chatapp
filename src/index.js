185b0074209e0415c339876da21f089c40de6ada
mikaelgo <mikaelgo@hotmail.com>
2019-04-24 18:58:00 +0300

    import main.scss in index.js. main.scss has the application font families and the zero css stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./components/scss/main.scss"
 import App from "./components/App";
 import { Provider } from "react-redux";
 import {store} from "./components/StoreManager"
